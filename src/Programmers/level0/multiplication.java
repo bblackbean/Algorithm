24cae8dda5caee48f57eaa6b6f3021f855a2b875
bblackbean <smart13210@gmail.com>
2022-10-04 20:06:30 +0900

    22.10.04

---
+++ src/Programmers/level0/multiplication.java
@@ -0,0 +1,10 @@
+package Programmers.level0;
+
+public class multiplication {
+	 public int solution(int num1, int num2) {
+	        int answer = 0;
+	        
+	        answer = num1 * num2;   
+	        return answer;
+	    }
+}
